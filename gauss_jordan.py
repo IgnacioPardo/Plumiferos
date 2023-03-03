9d205af3258c2f4901f09cdedd0fc2e633fbe368
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2023-03-03 11:16:48 -0300

    Create gauss_jordan.py

---
+++ gauss_jordan.py
@@ -0,0 +1,25 @@
+# Gauss Jordan implementation
+
+import numpy as np
+
+def gauss_jordan(A, b):
+
+    """Solves the linear system Ax = b using Gauss-Jordan elimination.
+    """
+
+    # Combine A and b into a single matrix
+    Ab = np.hstack((A, b))
+
+    # Perform Gauss-Jordan elimination
+    for i in range(Ab.shape[0]):
+        # Divide row i by the diagonal element
+        Ab[i] /= Ab[i, i]
+        # Subtract the row i from all other rows
+        for j in range(Ab.shape[0]):
+            if i != j:
+                Ab[j] -= Ab[i] * Ab[j, i]
+
+    # Extract the solution
+    x = Ab[:, -1]
+
+    return x
