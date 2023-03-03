d5bf171f75bd2524f206b2e7188ecb0c5bba9bcc
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2023-03-03 14:12:36 -0300

    Update gauss_jordan.py

--- gauss_jordan.py
+++ gauss_jordan.py
@@ -26,3 +26,15 @@ def gauss_jordan(A, b):
     x = Ab[:, -1]
 
     return x
+
+def vector_sum(v1, v2):
+    return [v1[i] + v2[i] for i in range(len(v1))]
+
+def vector_sub(v1, v2):
+    return [v1[i] - v2[i] for i in range(len(v1))]
+
+def vector_scalar_mult(v, s):
+    return [s * v[i] for i in range(len(v))]
+
+def vector_dot(v1, v2):
+    return sum([v1[i] * v2[i] for i in range(len(v1))])
